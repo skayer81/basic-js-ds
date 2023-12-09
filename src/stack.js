16765bc6a73cecabc87a4475ec760bc76a829a86
Sergey <seleznev-krista@mail.ru>
2023-12-10 00:23:14 +0300

    feat: add Stack

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor(){
+    this.stack = [];
+  }
+  
+  push( element) {
+    this.stack.push(element);
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    if (this.stack.length === 0) return undefined;
+    return this.stack.pop();
+   // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    return this.stack[this.stack.length-1]
+   // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
