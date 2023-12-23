ecfe9c0f1faa9dc2b2161e9c294326687aa8bc1c
Sergey <seleznev-krista@mail.ru>
2023-12-23 06:40:12 +0300

    feat: refact of remove

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -25,21 +25,21 @@ class BinarySearchTree {
   }
 
   _searchElem(data){
-    if (this._curent.data === data) //{
-      return// this._curent;}
+    if (this._curent.data === data) return
     if (this._curent.data < data){
       if (!this._curent.rigth) {
-        this._curentSide = 'rigth'// = this._newElem(data);
+        this._parentRigth = false;
+        this._curentSide = 'rigth';
         return
       }
       this._parentRigth = true;
       this._parent = this._curent;
       this._curent = this._curent.rigth;
-    //  this._searchElem(data)
     }
     else {
       if (!this._curent.left) {
-        this._curentSide = 'left'// = this._newElem(data);
+        this._curentSide = 'left'
+        this._parentRigth = true;
         return
       }
       this._parentRigth = false;
@@ -70,7 +70,6 @@ class BinarySearchTree {
   }
 
   _remove( result){
-    console.log(result,'узел для удаления')
     if (!result.left && !result.rigth){
       if(this._parentRigth) this._parent.rigth = null
       else this._parent.left = null
@@ -84,71 +83,35 @@ class BinarySearchTree {
       else this._parent.left = result.rigth
     }
     if (result.left && result.rigth){
-     // console.debug('для удаления', result)
-     // console.debug('min', this._findMin(result.rigth));
       let result = this._curent;
+      this._parent = result;
       this._curent = result.rigth;
-      this._searchElem(-Infinity);
+      this._searchElem(-1000);
       let curentMin = this._curent
-      
-      console.log(curentMin, 'минималка')
-     //let result = this._find(data);
       result.data = curentMin.data;
-    // this._find(data, curentMin);
       this._remove(curentMin);
     }
   }
 
   remove(data) {
     this._curent = this._root;
-   // let result = 
     this._searchElem(data);
-    if (this._curent.data === data) return this._remove(this._curent);//this._curent;
-//
-    // console.log
-    // if (!result) return;
-   // console.debug('remove',result)
-    
-
+    if (this._curent.data === data) return this._remove(this._curent);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this._curent = this._root;
+    this._searchElem(-Infinity);
+    return this._curent.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this._curent = this._root;
+    this._searchElem(Infinity);
+    return this._curent.data;
   }
 }
 
 module.exports = {
   BinarySearchTree
 };
-
-
-const tree = new BinarySearchTree();
-tree.add(9);
-tree.add(14);
-tree.add(2);
-tree.add(6);
-tree.add(128);
-tree.add(8);
-tree.add(31);
-tree.add(54);
-tree.add(1);
-//      console.debug('до удаления 14',tree)
-//tree.remove(14);
-   //   console.debug('после удаления 14',tree)
-// tree.remove(8);
- tree.remove(9);
-// assert.strictEqual(tree.has(14), false);
-// assert.strictEqual(tree.has(8), false);
-// assert.strictEqual(tree.has(9), false);
-// assert.strictEqual(tree.has(2), true);
-// assert.strictEqual(tree.has(6), true);
-// assert.strictEqual(tree.has(128), true);
-// assert.strictEqual(tree.has(31), true);
-// assert.strictEqual(tree.has(54), true);
-// assert.strictEqual(tree.has(1), true);
